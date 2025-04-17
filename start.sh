28d1ff9cb9cac0b23fe43421ededd6bff9bb4bce
bpmb82 <boudewijnbeckers@gmail.com>
2025-04-17 20:35:08 +0200

    Added README Improved on the startup script and the usage of env vars

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 output_prefix="server"
-default_port="5432"
 max_connections="${MAXCONN:-100}"
-check_port="8008"
+check_port="${CHECK_PORT:-8008}"
 
 generate_server_block() {
   local input_string="$1"
@@ -11,15 +10,15 @@ generate_server_block() {
   local tmpfile=$(mktemp)
   IFS=',' read -ra servers <<< "$input_string"
   for server_info in "${servers[@]}"; do
-    IFS=':' read -r hostname ip_address <<< "$server_info"
-    echo "    $output_prefix $hostname $ip_address:$default_port maxconn $max_connections check port $check_port" >> "${tmpfile}"
+    IFS=':' read -r hostname ip_address port <<< "$server_info"
+    echo "    $output_prefix $hostname $ip_address:${port:-5432} maxconn $max_connections check port $check_port" >> "${tmpfile}"
   done
   cat "$tmpfile"
 }
 
 if ! [ -f /etc/haproxy/haproxy.cfg ]; then
-  export PRIMARY_SERVERS="$(generate_server_block ${PRIMARIES})"
-  export STANDBY_SERVERS="$(generate_server_block ${STANDBYS})"
+  export PRIMARIES="$(generate_server_block ${PRIMARY_SERVERS})"
+  export STANDBYS="$(generate_server_block ${STANDBY_SERVERS})"
 
   envsubst < /etc/haproxy/haproxy.cfg.template > /etc/haproxy/haproxy.cfg
 fi
